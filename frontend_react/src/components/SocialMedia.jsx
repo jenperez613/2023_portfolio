3c2643a184ae1a718dbe3e986914d72b5027b82e
Jen Perez <jenperez613@icloud.com>
2023-06-05 10:20:18 -0400

    adding proper social links

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import { BsTwitter, BsInstagram } from 'react-icons/bs'
-import { FaFacebookF } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const SocialMedia = () => (
   <div className='app__social'>
     <div>
-      <BsTwitter />
+      <a
+        href='https://www.linkedin.com/in/jenniferramerperez/'
+        target='_blank'>
+        <FaLinkedin />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href='https://github.com/jenperez613' target='_blank'>
+        <FaGithub />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a href='https://www.instagram.com/jenperezzz/' target='_blank'>
+        <BsInstagram />
+      </a>
     </div>
   </div>
 )
